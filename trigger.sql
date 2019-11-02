b25e48a21a1a229fef39d51cfe43663cc9155c2b
dialvehid <skdiego27@gmail.com>
2019-11-02 10:38:05 -0600

    creacion de trigger

--- trigger.sql
+++ trigger.sql
@@ -5,7 +5,18 @@ as
 begin
   update aviones
   set ndisponibles= ndisponibles-1
-  where cod=inserted.cod
+  where cod=(select v.avion
+    from vuelo as v
+   where v.cod=inserted.id_vuelo)
 
-  
+if inserted.fecha=(select v.fsale
+                    from vuelo as v
+                    where v.cod=inseted.id_vuelo)
+    update embarque
+    set costo_f=costo-(costo*0.2)
+    where embarque.cod=inserted.cod
+else
+    update embarque
+    set costo_f=costo
+    where embarque.cod=inserted.cod
 end;
